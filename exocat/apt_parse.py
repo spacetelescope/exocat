ed4725fc18c09cb0556cae443ef32027fead45e0
Catherine A. Martlin <cmartlin@stsci.edu>
2020-11-23 14:53:01 -0500

    Created apt_parse.py and added document string

---
+++ exocat/apt_parse.py
@@ -0,0 +1,16 @@
+"""Downloads Phase II APT files to parse for data needed to fill 
+out the ExoCat table. 
+
+
+Authors
+-------
+    - Catherine Martlin, 2020
+Use
+---
+    This module is intended to be called by ``parse_apt.py``
+    as part of the ExoCat pipeline.
+Notes
+-----
+    We currently have a list of proposals hardcoded - may need to 
+    update in the future. 
+"""
