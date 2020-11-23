546b88d5cbf91aff2ff5821642f00c5c991de9e2
Catherine A. Martlin <cmartlin@stsci.edu>
2020-11-23 15:09:46 -0500

    Added a fetch_apt function to download the apt version of the proposal

--- exocat/apt_parse.py
+++ exocat/apt_parse.py
@@ -14,3 +14,38 @@ Notes
     We currently have a list of proposals hardcoded - may need to 
     update in the future. 
 """
+
+import urllib.request 
+import urllib.error
+import urllib.parse
+
+def fetch_apt(proposal_number = '15469'):
+	"""Downloads and saves the APT version of a proposal. 
+
+	Parameters
+	----------
+	proposal_number : string
+		The proposal number
+
+	Returns
+	-------
+	True or False : binary
+		True if the html request succeeds
+		False if the html request fails
+	"""
+
+    webpage = ('http://www.stsci.edu/hst/phase2-public/{}.apt'.format(proposal_number)) 
+    try:
+        response = urllib.request.urlopen(webpage)
+        html = response.read()
+    except urllib.error.HTTPError:
+        #logging.info('Error: Could not retrieve %s' % file)
+        return False
+    
+    filename = proposal_number + '.apt'
+    
+    f = open(filename, "wb")
+    f.write(html)
+    f.close()
+    
+    return True
