9308dad1aed60daeebbd03f6012e3ba5ceb111be
Catherine A. Martlin <cmartlin@stsci.edu>
2020-12-23 13:15:07 -0500

    Updates to finish crafting a full exocat table

--- exocat/apt_parse.py
+++ exocat/apt_parse.py
@@ -14,6 +14,7 @@ Notes
     We currently have a list of proposals hardcoded - may need to 
     update in the future. 
 """
+from datetime import datetime
 import numpy as np 
 import pandas as pd
 import urllib.request 
@@ -60,7 +61,13 @@ def read_apt(proposal_number = '15469'):
 	----------
 	proposal_number : string
 		The proposal number
+
+	Returns
+	-------
+	save_path : string
+		Path the final exocat table is saved at as a csv file.
 	"""
+	
 	apt_file = proposal_number + '.apt'
 
 	#We are using XML to read in the file
@@ -89,13 +96,15 @@ def read_apt(proposal_number = '15469'):
 	    phase_start.append(element.get('Start'))
 	    phase_end.append(element.get('End'))
 
+	exp_num = []
 	target_name_exposure = []
 	spectral_element = []
 	label = []
 	for element in exposures_info:
-	    target_name_exposure.append(element.get('TargetName'))
-	    spectral_element.append(element.get('SpElement'))
-	    label.append(element.get('Label'))
+    	exp_num.append(element.get('Number'))
+    	target_name_exposure.append(element.get('TargetName'))
+    	spectral_element.append(element.get('SpElement'))
+    	label.append(element.get('Label'))
 
 	status = []
 	number = []
@@ -113,35 +122,76 @@ def read_apt(proposal_number = '15469'):
 	for element in targets_info:
 	    target_name.append(element.get('Name'))
 
-	# Create dataframes from the different sections: 
+	# create dataframes from the different sections: 
 	target_name_exposure = np.array(target_name_exposure)
 	spectral_element = np.array(spectral_element)
-	exposures = zip(target_name_exposure, spectral_element)
-	exposure_df = pd.DataFrame((exposures), columns=['target_name_exposure','spectral_element'])
+	exp_num = np.array(exp_num)
+	exposure_df = pd.DataFrame(list(zip(exp_num, target_name_exposure, spectral_element)), columns=['exposure_number','target_name_exposure','spectral_element'])
 
 	# Remove aquisition exposures:
 	indexNames = exposure_df[exposure_df['spectral_element'].str.contains('F')].index
 	exposure_df.drop(indexNames , inplace=True)
+	exposure_df = exposure_df.reset_index()
+	exposure_df = exposure_df.drop(['index'], axis=1)
 
 	target_name_obs = np.array(target_name_obs)
 	NumberOfOrbits = np.array(NumberOfOrbits)
-	obs = zip(target_name_obs,NumberOfOrbits)
-	observations_df = pd.DataFrame((obs), columns=['target_name_obs', 'number_orbits'])
+	observations_df = pd.DataFrame(list(zip(target_name_obs,NumberOfOrbits)), columns=['target_name_obs', 'number_orbits'])
 
 	rate = np.array(rate)
 	direction = np.array(direction)
-	sp_scan = zip(rate, direction)
-	sp_scan_df = pd.DataFrame((sp_scan),columns=['rate','direction'])
+	sp_scan_df = pd.DataFrame(list(zip(rate, direction)),columns=['rate','direction'])
 
 	phase_start = np.array(phase_start)
 	phase_end = np.array(phase_end)
-	phases = zip(phase_start, phase_end)
-	phase_df = pd.DataFrame((phases), columns=['phase_start', 'phase_end'])
+	phase_df = pd.DataFrame(list(zip(phase_start, phase_end)), columns=['phase_start', 'phase_end'])
 
 	target_name = np.array(target_name)
 	target_df = pd.DataFrame((target_name), columns = ['target_name'])
 
-# Still need to gather alternate name from Targets/FixedTarget/AlternateNames
-# and Date from Visits/Visit/ToolData/ToolDataItem
+	status = np.array(status)
+	number = np.array(number)
+	visits_info_df = pd.DataFrame(list(zip(status, number)), columns = ['status', 'visit_number'])
 
+	#fix table with orbit numbers:
+	if len(visits_info_df.index) != len(observations_df.index):
+	    visits_no_fail = visits_info_df.copy()
+	    failed_visits = visits_info_df[visits_info_df['status'].str.contains('failed')].index
+	    visits_no_fail.drop(failed_visits, inplace=True)
+	    visits_no_fail = visits_no_fail.reset_index()
+	    obs_and_visits_df = pd.concat([observations_df, visits_no_fail], axis=1)
+	    obs_and_visits_df = obs_and_visits_df.drop(['index'], axis=1)
+	    obs_and_visits_df = obs_and_visits_df.drop(['target_name_obs'], axis=1)
+	    obs_and_visits_df = obs_and_visits_df.drop(['status'], axis=1)
+	else:
+	    obs_and_visits_df = pd.concat([observations_df, visits_info_df], axis=1)
+	    obs_and_visits_df = obs_and_visits_df.drop(['target_name_obs'], axis=1)
+	    obs_and_visits_df = obs_and_visits_df.drop(['status'], axis=1)
+	    
+	#combine tables: 
+	combine1 = pd.concat([exposure_df,sp_scan_df], axis=1)
+	reduce1 = combine1[combine1["exposure_number"] == '2']
+	reduce1 = reduce1.reset_index()
+	reduce1 = reduce1.drop(['index'], axis=1)
+	combine2 = pd.concat([reduce1, phase_df, visits_info_df], axis=1)
+	combine2 = combine2.drop(['exposure_number'], axis=1)
 
+	#add column with proposal number:
+	total_entries = len(combine2.index)
+	proposal_list = []
+	for x in range(total_entries):
+	    proposal_list.append(proposal_number)
+	prop_list = np.array(proposal_list)
+	prop_df = pd.DataFrame((prop_list), columns=['proposal_number'])
+
+	#final dataframe
+	add_orbits = pd.merge(combine2, obs_and_visits_df, on='visit_number')
+	final_table = pd.concat([combine2, prop_df], axis=1)
+
+	#save final_table
+	now = datetime.now()
+	dt_string = now.strftime("%d_%m_%Y_%H_%M_%S")
+	save_path = proposal_number + '_exocat_' + dt_string + '.csv'
+	final_table.to_csv(save_path)
+
+	return save_path
