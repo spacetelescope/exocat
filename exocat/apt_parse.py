7d66b650f71af9e276f16616f7bb824b1b1442fc
Catherine A. Martlin <cmartlin@stsci.edu>
2021-01-20 10:43:54 -0500

    Adding current version of visit status reader which still needs to be flushed out completely

--- exocat/apt_parse.py
+++ exocat/apt_parse.py
@@ -245,6 +245,55 @@ def read_apt(proposal_number = '15469'):
 
     return save_path
 
+def read_visit_status(proposal_number = '15469'):
+    """Uses the visit status file to read in the visit date/time
+
+    Parameters
+    ----------
+    proposal_number : string
+        The proposal number
+
+    Returns
+    -------
+
+    """
+    proposal_number = str(proposal_number)
+    filename = proposal_number + '_visit_status.xml'
+
+    #We are using XML to read in the file
+    tree = ET.parse(filename)
+    entire_file = tree.getroot()
+    print(entire_file)
+    
+    visits = entire_file.findall('visit')
+    statuses = entire_file.findall('visit/status')
+    targets = entire_file.findall('visit/target')
+    startTimes = entire_file.findall('visit/startTime')
+    endTimes = entire_file.findall('visit/endTime')
+    
+    visit_list = []
+    for visit in visits: 
+        visit_list.append(visit.get('id'))
+
+    status_list = []
+    for elem in statuses: 
+        status_list.append(elem.text)
+
+    targets_list = []
+    for elem in targets: 
+        targets_list.append(elem.text)
+
+    startTimes_list = []
+    for elem in startTimes: 
+        startTimes_list.append(elem.text)
+
+    endTimes_list = []
+    for elem in endTimes: 
+        endTimes_list.append(elem.text)
+        
+    all_info = list(zip(visit_list, status_list, targets_list, startTimes_list, endTimes_list))
+
+
 def main():
     """The main function that will run a list of proposals. Also sets
     up and logs timing and information of each run.
