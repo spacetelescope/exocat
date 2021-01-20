ad2a244f0a055c212aaec78813b6dbe69ed45fae
Catherine A. Martlin <cmartlin@stsci.edu>
2021-01-20 10:40:53 -0500

    Updating visit status to get xml instead of html file

--- exocat/apt_parse.py
+++ exocat/apt_parse.py
@@ -85,20 +85,21 @@ def fetch_apt(proposal_number = '15469'):
 
 def fetch_visit_status(proposal_number='15469'):
     proposal_number = str(proposal_number)
-    webpage = ('https://www.stsci.edu/cgi-bin/get-visit-status?id={}&markupFormat=html&observatory=HST&pi=1'.format(proposal_number))
+    
+    webpage = ('https://www.stsci.edu/cgi-bin/get-visit-status?id={}&markupFormat=xml&observatory=HST'.format(proposal_number)) 
     try:
         response = urllib.request.urlopen(webpage)
         html = response.read()
     except urllib.error.HTTPError:
         #logging.info('Error: Could not retrieve %s' % file)
         return False
-
-    filename = proposal_number + '_visit_status.html'
-
+    
+    filename = proposal_number + '_visit_status.xml'
+    
     f = open(filename, "wb")
     f.write(html)
     f.close()
-
+    
     return True
 
 def read_apt(proposal_number = '15469'):
